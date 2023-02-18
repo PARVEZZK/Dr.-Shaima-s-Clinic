380e145fc9c2c318720c7aa8cea4a568aac70abc
parvez khan <parvezzkhan688@gmail.com>
2023-02-18 15:37:13 +0530

    initail

---
+++ shaima_clinicts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shaima_clinicts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
